f99d4d374b34cede3c8abc061b563eacb7dc1d4a
admin <admin@DESKTOP-0CCABRD>
2021-09-17 20:43:06 +0800

    添加项目文件。

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,22 @@
+// ConsoleApplication4.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "person.hpp"
+#include <iostream>
+using namespace std;
+
+
+void test01()
+{
+	Person <string, int>p("12", 88);
+	p.ShowPerson();
+}
+int main()
+{
+	test01();
+}
+
+
+
+
